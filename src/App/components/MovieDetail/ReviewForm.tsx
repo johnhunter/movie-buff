e93142d60efbc5358fd174ed63eac9339bc866e5
John Hunter <john@johnhunter.info>
2023-09-18 19:02:39 +0100

    Prevent adding reviews before movie has been watched

--- src/App/components/MovieDetail/ReviewForm.tsx
+++ src/App/components/MovieDetail/ReviewForm.tsx
@@ -2,12 +2,20 @@ import { FC, useState } from 'react';
 import css from './MovieDetail.module.css';
 
 interface ReviewForm {
+  disabled?: boolean;
   initialValue?: string;
   onSubmit: (value: string) => void;
 }
 
-const ReviewForm: FC<ReviewForm> = ({ initialValue = '', onSubmit }) => {
+const ReviewForm: FC<ReviewForm> = ({
+  initialValue = '',
+  onSubmit,
+  disabled,
+}) => {
   const [value, setValue] = useState(initialValue);
+  const placeholder = disabled
+    ? 'Mark the movie as viewed before adding a review'
+    : 'Add your own review for this movie';
 
   return (
     <div className={css.review}>
@@ -15,11 +23,16 @@ const ReviewForm: FC<ReviewForm> = ({ initialValue = '', onSubmit }) => {
       <textarea
         value={value}
         onChange={(e) => setValue(e.currentTarget.value)}
-        placeholder="Add your own review for this movie"
+        placeholder={placeholder}
+        disabled={disabled}
       ></textarea>
       <div>
-        <button onClick={() => onSubmit(value)}>Save review</button>{' '}
-        <button onClick={() => setValue(initialValue)}>Cancel changes</button>
+        <button disabled={disabled} onClick={() => onSubmit(value)}>
+          Save review
+        </button>{' '}
+        <button disabled={disabled} onClick={() => setValue(initialValue)}>
+          Cancel changes
+        </button>
       </div>
     </div>
   );
